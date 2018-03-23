9173bfcb0cabd9be24ea5d8fd9e09355d1721359
Ellis Percival <flyte@failcode.co.uk>
2018-03-23 10:17:56 +0000

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rancher-tools',
-    version='0.0.1',
+    version='0.0.2',
     install_requires='requests',
     py_modules=['rancher_tools'],
     license='MIT'
